b36827a909dd7da9437280482d5384917cde10da
Michaël De Laborde <michael.delaborde@abtasty.com>
2020-05-14 16:14:22 +0200

    Changed poster

--- src/components/Folio/utils/config.js
+++ src/components/Folio/utils/config.js
@@ -3,7 +3,7 @@ import d2r from './d2r.js'
 export default {
     camera: {
         fov: 43.6,
-        fovMobile: 56.6,
+        fovMobile: 60.6,
         near: 0.01,
         far: 1000,
         position: {
